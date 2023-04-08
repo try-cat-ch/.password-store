25ffaf90e2c36afea495025f708b97ba693952be
Sergey <try-cat-ch@gmail.com>
2023-04-08 06:00:31 +0000

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -6,3 +6,5 @@ echo "export TF_VAR_tf_yandex_cloud_api_token="$(pass ls terraform/terraform-yan
 echo "export TF_VAR_tf_yandex_cloud_id_dev="$(pass ls terraform/terraform-yandex/yandex_cloud_id_dev)"" >> ~/.bashrc
 echo "export TF_VAR_tf_yandex_cloud_folder_dev_default="$(pass ls terraform/terraform-yandex/yandex_cloud_folder_dev-devault)"" >> ~/.bashrc
 
+echo "export TF_VAR_tf_yandex_tinder_network_cloud_id="$(pass ls telegram-cloud-manager/tinder/main/cloud_id)"" >> ~/.bashrc
+echo "export TF_VAR_tf_yandex_tinder_network_main_folder_id="$(pass ls telegram-cloud-manager/tinder/main/folder_id)"" >> ~/.bashrc
